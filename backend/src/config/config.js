423678ee2fb701065e4cba4daa86e66cd9ee2a7c
blogkeren <blogkeren.web.id@gmail.com>
2025-12-06 17:32:19 +0700

    Update config.js

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -6,5 +6,8 @@ export const config = {
   port: process.env.PORT || 3000,
   kitanontonBaseUrl: process.env.KITANONTON_BASE_URL || 'https://kitanonton2.live',
   rebahinBaseUrl: process.env.REBAHIN_BASE_URL || 'https://rebahinxxi3.fit',
-  userAgent: process.env.USER_AGENT || 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36'
+  userAgent: process.env.USER_AGENT || 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36',
+  // Tambahan baru untuk Cache
+  cloudflareWorkerUrl: process.env.CLOUDFLARE_WORKER_URL,
+  cloudflareApiSecret: process.env.CLOUDFLARE_API_SECRET
 };
